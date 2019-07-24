0cd34eeade785094449e48a52d41e8e1910e1498
github username <github email address>
2019-07-24 10:46:05 -0400

    deliverable #1 complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// document.addEventListener("DOMContentLoaded", function() {});
+
+//deliverable #1 : INDEX
+// see a list of courses
+
+const courseContainer = document.querySelector("#course-container")
+
+function getAllCourses(){
+  fetch('https://warm-shore-17060.herokuapp.com/api/v1/users/57/courses')
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(courses){
+    courses.forEach(function(course){
+    const courseList = document.createElement("p")
+    courseList.innerHTML = course.name
+    courseContainer.appendChild(courseList)
+    })
+  })
+
+}
+
+getAllCourses()
