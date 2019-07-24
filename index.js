c35bb4732def72beac9c7480f0335851a1ab47b3
Alexander Riccio <test35965+learn@gmail.com>
2019-07-24 12:07:48 -0400

    solve, at very LAST minute

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const MY_API_BASE_URL = `https://warm-shore-17060.herokuapp.com/api/v1/users/50`;
 const COURSE_DETAIL_BUTTON_CLASS_NAME = "see-course-detail-button";
 const STUDENT_DETAIL_LIST_ELEMENT_CLASS_NAME = "student-detail-list-element-class";
+const STUDENT_DETAIL_LIST_ID = "student-detail-list";
 const STUDENT_FORM_EDIT_FORM = "student-form-edit-form";
 const STUDENT_EDIT_PERCENTAGE_INPUT_FIELD = "student-edit-percentage-input-field";
 const STUDENT_EDIT_FORM_CURRENT_STUDENT = "student-edit-form-student-id";
@@ -19,6 +20,10 @@ function coursesURL() {
 
 function coursesDetailURL(id) {
     return `${coursesURL()}/${id}`;
+}
+
+function studentDetailURL(studentID) {
+    return `${MY_API_BASE_URL}/students/${studentID}`;
 }
 
 function createCourseDiv(course) {
@@ -92,8 +97,10 @@ function renderSingleStudentListItem(student) {
 }
 
 function renderStudentList(course) {
-    const studentListElement = document.createElement("ul");
 
+    const studentListElement = document.createElement("ul");
+    studentListElement.dataset.courseID = course.id;
+    studentListElement.id = STUDENT_DETAIL_LIST_ID;
     for (let i = 0; i < course.students.length; i++) {
         const singleStudentListItem = renderSingleStudentListItem(course.students[i]);
         studentListElement.appendChild(singleStudentListItem);
@@ -101,9 +108,29 @@ function renderStudentList(course) {
     return studentListElement;
 }
 
+function patchStudentPercentage(studentID, newPercentage) {
+    fetch(studentDetailURL(studentID), {
+        method: 'PATCH',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+            id: studentID,
+            percentage: newPercentage
+        })
+    }).then(resp => resp.json()).then(resp => {
+        console.log("patch response: " +resp);
+        debugger;
+    })
+}
+
 function editStudentSubmitHandler(event) {
     console.log(event);
-    console.log("new student percentage value: " + event.target.children[0].value)
+    console.log("new student percentage value: " + event.target.children[0].value);
+    console.log("student ID: " + event.target.parentElement.dataset.id);
+
+    patchStudentPercentage(event.target.parentElement.dataset.id, event.target.children[0].value);
+    event.preventDefault();
     debugger;
 }
 
@@ -111,7 +138,7 @@ function editStudentSubmitHandler(event) {
 function renderStudentForm(studentLI) {
     studentForm.innerHTML = "";
     const newFormStudentName = document.createElement("h3");
-    newFormStudentName.id = 
+    // newFormStudentName.id = STUDENT_EDIT_FORM_CURRENT_STUDENT;
     newFormStudentName.innerText = studentLI.dataset.name;
     studentForm.appendChild(newFormStudentName);
 
@@ -143,6 +170,7 @@ function renderStudentForm(studentLI) {
 
     const newFormStudentEditButton = document.createElement("button");
     newFormStudentEditButton.innerText = "Edit";
+
     newFormStudentEditPercentage.appendChild(newFormStudentEditButton);
 
     studentForm.dataset.id = studentLI.dataset.id;
@@ -172,6 +200,7 @@ function attachCourseDetailListButtons() {
 
 function renderCourseDetailFromFetch(course) {
     const studentListElement = renderStudentList(course);
+    courseDetail.innerHTML = "";
     courseDetail.appendChild(studentListElement);
     attachCourseDetailListButtons();
 }
